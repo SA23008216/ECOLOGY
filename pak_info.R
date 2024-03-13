12c39ef927c401298224ba565b0664ddaa2ebdf5
SA23008216 <3310166542@qq.com>
2024-03-13 11:55:57 +0800

    SA23008216-wangcijian-homework2

---
+++ pak_info.R
@@ -0,0 +1,22 @@
+#HEADER----------------------------------------------------
+#
+#Author: wangcijian
+#Copyright   Copyright 2024-wangcijian
+#Email:3310166542@qq.com
+#
+#Date:2024-03-13
+#
+#Script Name: pak_info.R
+#
+#Script Description:
+#install.packages("tidyverse") ##安装tidyverse包
+#library(tidyverse) ##加载tidyverse包
+#packageDescription("tidyverse") ##获取tidyverse包的信息
+#help("tidyverse") ##获取有关 tidyverse 包的帮助文档
+#
+#SETUP ----------------------------------------------------
+
+install.packages("tidyverse") ##安装tidyverse包
+library(tidyverse) ##加载tidyverse包
+packageDescription("tidyverse") ##获取tidyverse包的信息
+help("tidyverse") ##获取有关 tidyverse 包的帮助文档
